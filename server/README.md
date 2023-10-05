f5ce320579bcf29c9f696d8a2b2ba373df6539ca
Venux <arifmp40@gmail.com>
2023-10-05 18:11:11 +0600

    init

--- server/README.md
+++
@@ -1,2 +0,0 @@
-# dev-api
-Try To Create Rest api for dev shop ecoomarch website Explore mongose
--- server/models/productcategory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const ProductCategorySchema = new mongoose.Schema({
-    category: {type: String, required: true},
-    image: {type: String, required: true},
-},
-{timestamps: true})
-module.exports = mongoose.model('ProductCategory', ProductCategorySchema)
